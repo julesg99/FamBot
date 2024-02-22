2084b8b5f68b73c8ba81ffdf5920ba04239ab2cd
julesg99 <juliangarver03@gmail.com>
2024-02-21 22:20:28 -0600

    nomination command working

--- commands/suffrage/nominate.js
+++ commands/suffrage/nominate.js
@@ -1,15 +1,36 @@
 const { SlashCommandBuilder } = require('discord.js');
+const fs = require('node:fs');
+const path = './data/filmNight1.json';
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('nominate')
         .setDescription('Nominate a movie.')
         .addStringOption(option =>
-            option.setName('movie')
-                .setDescription('The letterboxd url for your movie.')
+            option.setName('title')
+                .setDescription('The title of your movie.')
+                .setRequired(true))
+        .addStringOption(option =>
+            option.setName('url')
+                .setDescription('The Letterboxd URL of your movie.')
                 .setRequired(true)),
     async execute(interaction) {
+        const title = interaction.options.getString('title');
+        const url = interaction.options.getString('url');
         
+        fs.readFile(path, 'utf8', (err, file) => {
+            if (err) {
+                console.error(err);
+                data = [];
+            } else {
+                let data = JSON.parse(file);
+            
+                data.push({ title: title, url: url });
+                fs.writeFile(path, JSON.stringify(data), (err) => {
+                    if (err) throw err;                    
+                    interaction.reply(`Nomination received for **[${title}](${url})**!`);
+                });
+            }
+        }); 
     }
-}
-
+};
