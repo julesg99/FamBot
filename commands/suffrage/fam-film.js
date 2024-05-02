3164e837fb5cb8fef8b5ca1b476dfa651c454bff
BLGFQY <blgfqy@mail.umkc.edu>
2024-05-01 23:18:29 -0500

    small fixes

--- commands/suffrage/fam-film.js
+++ commands/suffrage/fam-film.js
@@ -10,7 +10,7 @@ module.exports = {
                 .setDescription('The number of participants.')
                 .setRequired(true)),
     async execute(interaction) {
-        let { famFilmCount } = require('../../config.json');
+        const { famFilmCount } = require('../../config.json');
         const path = `./data/famFilm${famFilmCount}.json`;
         const participants = interaction.options.getString('participants');
         const data = {
