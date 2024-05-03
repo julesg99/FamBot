8dbcb69e9f9492705acec9d6abfcdc86bed6eb04
julesg99 <juliangarver03@gmail.com>
2024-05-02 22:59:43 -0500

    fixed and refactored nomineeDisplay

--- commands/suffrage/status.js
+++ commands/suffrage/status.js
@@ -1,38 +1,11 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
-const {
-  EmbedBuilder,
-  TextInputBuilder,
-  ActionRowBuilder,
-} = require("discord.js");
-const fs = require("node:fs");
-const { famFilmCount } = require("../../config.json");
-const nomineeDisplay = require("../../lib/nomineeDisplay.js")
+const { nomineeDisplay } = require("../../lib/nomineeDisplay.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("status")
     .setDescription("Get the status of fam film night"),
   async execute(interaction) {
-    // const voteGrid = nomineeDisplay();
-    // interaction.reply({ embeds: [voteGrid] })
-
-    const path = `./data/famFilm${famFilmCount}.json`;
-    let description = "";
-
-    fs.readFile(path, (err, file) => {
-      if (err) {
-        console.log(err);
-      } else {
-        let data = JSON.parse(file);
-        for (const nominee of data.nominations) {
-          description += `**[${nominee.title}](${nominee.url})**\n`;
-        }
-        const voteGrid = new EmbedBuilder()
-          .setTitle(`Nominations for Fam Film Night #${famFilmCount}`)
-          .setDescription(description);
-
-        interaction.reply({ embeds: [voteGrid] });
-      }
-    });
+    interaction.reply({ embeds: [nomineeDisplay()] })
   },
 };
