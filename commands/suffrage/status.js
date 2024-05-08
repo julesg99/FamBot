04603606c3473b7a580de897b9d7bc6433f980d6
julesg99 <juliangarver03@gmail.com>
2024-05-07 22:49:32 -0500

    implement new getter functions

--- commands/suffrage/status.js
+++ commands/suffrage/status.js
@@ -2,21 +2,22 @@ const {
   EmbedBuilder
 } = require("discord.js");
 const { SlashCommandBuilder } = require("@discordjs/builders");
-const { readFamFilmFile } = require("../../lib/readFile.js");
-const { famFilmCount } = require("../../config.json");
+const { selectNominations } = require("../../lib/nominations.js");
+const { selectFilmNightNumber } = require("../../lib/filmNight.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("status")
     .setDescription("Get the status of fam film night"),
   async execute(interaction) {
-    const fileData = readFamFilmFile();
+    const nominations = await selectNominations();
+    const famFilmCount = await selectFilmNightNumber();
 
     const voteGrid = new EmbedBuilder()
       .setTitle(`Nominations for Fam Film Night #${famFilmCount}`);
     let description = "";
 
-    for (const nominee of fileData.nominations){
+    for (const nominee of nominations){
       description += `**[${nominee.title}](${nominee.url})**\n`;
     }
     voteGrid.setDescription(description);
