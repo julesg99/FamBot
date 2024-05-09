543ebe06c1c6d26ea0b7a94063ff813fb07c962c
julesg99 <juliangarver03@gmail.com>
2024-05-08 21:51:03 -0500

    create insert film night, update fam film cmd to use db

--- commands/suffrage/fam-film.js
+++ commands/suffrage/fam-film.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder } = require('discord.js');
 const fs = require('node:fs');
+const { insertFilmNight } = require('../../lib/filmNight');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -10,20 +11,8 @@ module.exports = {
                 .setDescription('The number of participants.')
                 .setRequired(true)),
     async execute(interaction) {
-        const { famFilmCount } = require('../../config.json');
-        const path = `./data/famFilm${famFilmCount}.json`;
-        const participants = interaction.options.getString('participants');
-        const data = {
-            "famFilmNumber": famFilmCount,
-            "numParticipants": participants,
-            "nominations": []
-        };
-
-        fs.writeFile(path, JSON.stringify(data), (err) => {
-            if (err) throw err;
-            else {
-                interaction.reply(`Fam Film Night #${famFilmCount} has begun!\nAll ${participants} participants must nominate a movie using \`/nominate\``);
-            }
-        });
+        const participants = interaction.options.getString('participants');        
+        const { number } = await insertFilmNight(participants);
+        interaction.reply(`Fam Film Night #${number} has begun!\nAll ${participants} participants must nominate a movie using \`/nominate\``);
     }   
 };
