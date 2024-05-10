34a2185f050b7353160325b571eb5756344f219f
julesg99 <juliangarver03@gmail.com>
2024-05-09 22:51:20 -0500

    fixed query

--- commands/suffrage/status.js
+++ commands/suffrage/status.js
@@ -1,22 +1,20 @@
 const { EmbedBuilder } = require("discord.js");
 const { SlashCommandBuilder } = require("@discordjs/builders");
-const { selectNominations } = require("../../lib/nominations.js");
-const { selectFilmNightNumber } = require("../../lib/filmNight.js");
+const { selectCurrentFilmNight } = require("../../lib/filmNight.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("status")
     .setDescription("Get the status of fam film night"),
   async execute(interaction) {
-    const nominations = await selectNominations(["c420e331-9b00-4b25-8b9e-01c9a64f38af", "4c771d39-0798-4a96-8f34-cc545e6fae60"]);
-    const famFilmCount = await selectFilmNightNumber();
+    const filmNight = await selectCurrentFilmNight();
 
     const voteGrid = new EmbedBuilder()
-      .setTitle(`Nominations for Fam Film Night #${famFilmCount}`);
+      .setTitle(`Nominations for Fam Film Night #${filmNight.number}`);
     let description = "";
 
-    for (const nominee of nominations){
-      description += `**[${nominee.title}](${nominee.url})**\n`;
+    for (const nominee of filmNight.nominations){
+      description += `${nominee.participant.name}: **[${nominee.title}](${nominee.url})**\n`;
     }
     voteGrid.setDescription(description);
 
