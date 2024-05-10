04a09d0ad9fbb637ab952d038e7a8e8570d47022
julesg99 <juliangarver03@gmail.com>
2024-05-09 22:42:59 -0500

    rolling back proposed query pattern

--- commands/suffrage/fam-film.js
+++ commands/suffrage/fam-film.js
@@ -1,6 +1,5 @@
 const { SlashCommandBuilder } = require('discord.js');
-const fs = require('node:fs');
-const { insert } = require('../../lib/filmNight');
+const { insertFilmNight } = require('../../lib/filmNight');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -12,7 +11,7 @@ module.exports = {
                 .setRequired(true)),
     async execute(interaction) {
         const participants = interaction.options.getString('participants');        
-        const { number } = await insert.filmNight(participants);
+        const { number } = await insertFilmNight(participants);
         interaction.reply(`Fam Film Night #${number} has begun!\nAll ${participants} participants must nominate a movie using \`/nominate\``);
     }   
 };
