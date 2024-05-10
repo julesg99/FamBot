2c21cd6e85055c1c3987f2dfb8a3ad204b83e7a8
julesg99 <juliangarver03@gmail.com>
2024-05-09 21:24:00 -0500

    added currentFilmNight query, working on query pattern

--- commands/suffrage/fam-film.js
+++ commands/suffrage/fam-film.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder } = require('discord.js');
 const fs = require('node:fs');
-const { insertFilmNight } = require('../../lib/filmNight');
+const { insert } = require('../../lib/filmNight');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -12,7 +12,7 @@ module.exports = {
                 .setRequired(true)),
     async execute(interaction) {
         const participants = interaction.options.getString('participants');        
-        const { number } = await insertFilmNight(participants);
+        const { number } = await insert.filmNight(participants);
         interaction.reply(`Fam Film Night #${number} has begun!\nAll ${participants} participants must nominate a movie using \`/nominate\``);
     }   
 };
