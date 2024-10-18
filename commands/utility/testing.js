8d1708d8edcc303986a85676bcc32d8346ed7d87
Justin <Capstone451JM@protonmail.com>
2024-10-18 16:46:35 -0500

    WIP

---
+++ commands/utility/testing.js
@@ -0,0 +1,11 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("test")
+        .setDescription("testing stuff"),
+    async execute(interaction){
+
+        interaction.reply("testing finished");
+    }
+}
